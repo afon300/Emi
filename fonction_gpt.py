14d7cb561ee4832989e7f48a9985acd43422095b
afon30 <antoinefourneyron@gmail.com>
2024-02-16 00:30:14 +0100

    modif du main.py

--- fonction_gpt.py
+++ fonction_gpt.py
@@ -1,13 +1,15 @@
 import openai
+import os
     
 def GPT(prompt):
-  openai.api_key = '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'
-
+  with open('key.txt', 'r') as f:
+   key = f.read()
+  openai.api_key = key
   completion = openai.ChatCompletion.create(
     model="gpt-3.5-turbo", 
     messages=[{"role": "user", "content": prompt}]
   )
 
   reponse = (completion['choices'][0]['message']['content'])
-  
+    
   return reponse
--- main.py
+++ main.py
@@ -1,7 +1,20 @@
+import datetime
+import time
+
 from fonction_gpt import *
 from fonctions import *
 
+temps = datetime.datetime.now()
+if temps.hour == 8 and temps.minute == 0 and temps.second == 0:
+    cls()
+    prompt = ("Tu est un utilisateur de Twitter, tu parle à tes amis, nous somme le", temps, "dis bonjour à tout le monde et souhaite leur de passer une bonne journée")
+    reponse = GPT(prompt)
+    print(reponse)
+    time.sleep(1)
+
 cls()
-prompt = str(input("Entrez le prompt : "))
+prompt1 = "Tu est un utilisateur de Twitter, fais des messages courts en moyenne 100 caractères. caractère, tu parle à tes amis, nous somme le"
+prompt2 = "dis bonjour à tout le monde et souhaite leur de passer une bonne journée"
+prompt = prompt1 + " " + str(temps) + " " + prompt2
 reponse = GPT(prompt)
 print(reponse)
