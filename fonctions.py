770c5b4e932f8e4fd548f46bc0bcf6f325d86ecf
afon30 <antoinefourneyron@gmail.com>
2024-02-20 02:41:41 +0100

    gros ajouts !

--- fonctions.py
+++ fonctions.py
@@ -1,7 +1,37 @@
+import time
+import random
 import os
+import openai
+
+from librairies_dico import *
+
+################################################ BASIC FUNCTIONS ################################################
 
 def cls():
     os.system('cls' if os.name == 'nt' else 'clear')
+    
+def heure_aléatoire():
+    aléheure = random.randint(0, 23)
+    while (aléheure > 2 and aléheure < 8):
+        aléheure = random.randint(0, 23)
+    alémin = random.randint(0, 59)
+    heure_formatee = str(aléheure).zfill(2)
+    minute_formatee = str(alémin).zfill(2)
+    heure_aléatoire = heure_formatee + ":" + minute_formatee
+    return heure_aléatoire
+
+def minutes_aléatoire():
+    minutes_aléatoires1 = random.randint(0, 59)
+    minute_formatee1 = str(minutes_aléatoires1).zfill(2)
+    
+    minutes_aléatoires2 = random.randint(0, 59)
+    minute_formatee2 = str(minutes_aléatoires2).zfill(2)
+    
+    minutes_aléatoires3 = random.randint(0, 59)
+    minute_formatee3 = str(minutes_aléatoires3).zfill(2)
+    
+    return minute_formatee1, minute_formatee2, minute_formatee3
+
 
 def dechiffrer_cesar(message_chiffre):
     decalage = 1
@@ -20,3 +50,126 @@ def dechiffrer_cesar(message_chiffre):
             message_dechiffre += char
 
     return message_dechiffre
+
+def prompt():
+    prompt1 = "Tu est une utilisatrice de Twitter, fan d'anime, tu a 16 ans, agis comme si tu avais une conversation normal entre toi et ton groupe, tu es gentille et attentionné, tu es un peu timide, fais des messages courts en moyenne 120 caractères dans le style des anime, tu parler à la 2e personne du pluriel quand tu pause des question. caractère, tu parle à tes amis, nous somme le"
+    return prompt1
+################################################ API CHATGPT #################################################
+
+def gpt(prompt):
+    with open('key.txt', 'r') as f:
+        clé_chiffré = f.read()
+        clé_déchiffré = dechiffrer_cesar(clé_chiffré)
+    openai.api_key = clé_déchiffré
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo", 
+        messages=[{"role": "user", "content": prompt}]
+    )
+
+    reponse = (completion['choices'][0]['message']['content'])
+        
+    return reponse
+
+############################################### DAILY FUNCTIONS ##############################################
+
+
+def fonction_matin(jour, temps, prompt1):
+    dico_prompt_matin = dico_matin()            
+    pl = True
+    prompt2 = ""
+    while pl == True:
+        random_nb = random.randint(1, 13)
+        if random_nb == 3 and jour == 5:
+            pl = False
+        if random_nb == 4 and jour == 6:
+            pl = False
+        if ((random_nb == 7 or random_nb == 8) and (jour != 5 and jour != 6)):
+            pl = False
+        if random_nb == 12 and jour == 4:
+            pl = False
+        if random_nb == 10 and jour == 6:
+            pl = False
+        if ((random_nb != 3) and (random_nb != 4) and (random_nb != 7) and (random_nb != 8) and (random_nb != 12) and (random_nb != 10)):
+            pl = False
+                
+    for prompt_du_dico_matin, num in dico_prompt_matin.items():
+        if num == random_nb:
+            prompt2 = prompt_du_dico_matin
+            break
+    prompt = prompt1 + " " + str(temps) + " " + prompt2
+    reponse = gpt(prompt)
+    
+    return reponse
+
+def fonction_soir(jour, temps, prompt1):
+    dico_prompt_soir = dico_soir()            
+    pl = True
+    prompt2 = ""
+    while pl == True:
+        random_nb = random.randint(1, 18)
+        if random_nb == 16 and jour == 4:
+            pl = False
+        if random_nb == 17 and jour == 4:
+            pl = False
+        if random_nb != 16:
+            pl = False
+                
+    for prompt_du_dico_soir, num in dico_prompt_soir.items():
+        if num == random_nb:
+            prompt2 = prompt_du_dico_soir
+            break
+    prompt = prompt1 + " " + str(temps) + " " + prompt2
+    reponse = gpt(prompt)
+        
+    return reponse
+    
+def fonction_nuit(temps, prompt1):
+    dico_prompt_nuit = dico_nuit()            
+    random_nb = random.randint(1, 18)   
+    prompt2 = ""      
+    for prompt_du_dico_nuit, num in dico_prompt_nuit.items():
+        if num == random_nb:
+            prompt2 = prompt_du_dico_nuit
+            break
+    prompt = prompt1 + " " + str(temps) + " " + prompt2
+    reponse = gpt(prompt)
+    
+    return reponse
+
+def fonction_phrases_random(prompt1):
+    dico_phrases = dico_phrases()            
+    random_nb = random.randint(1, 76)   
+    prompt2 = ""      
+    for prompt_du_dico_random, num in dico_phrases.items():
+        if num == random_nb:
+            prompt2 = prompt_du_dico_random
+            break
+    prompt = prompt1 + " " + prompt2
+    reponse = gpt(prompt)
+    
+    return reponse
+
+def fonction_anime_random(prompt1):
+    dico_anime_question = dico_question_anime_name()   
+    dico_anime_nom = dico_nom_anime()  
+    
+    question_anime_aléatoire = random.randint(1, 15)        
+    nom_anime_aléatoire = random.randint(1, 52)
+    
+    question = ""
+    anime = ""    
+    for question_anime, num in dico_anime_question.items():
+        if num == question_anime_aléatoire:
+            question = question_anime
+            break
+
+    for anime, num in dico_anime_nom.items():
+        if num == nom_anime_aléatoire:
+            nom_anime = anime
+            break 
+        
+    prompt2 = question.format(anime=nom_anime)
+    prompt = prompt1 + " " + prompt2
+    reponse = gpt(prompt)
+    
+    return reponse
--- main.py
+++ main.py
@@ -1,49 +1,47 @@
 import datetime
+import schedule
 import time
 import random
 
-from api_gpt import *
 from fonctions import *
+from librairies_dico import *
+
+##################################################### TIME #####################################################
 
 temps = datetime.datetime.now()
 jour = temps.weekday()
-if temps.hour == 8 and temps.minute == 0 and temps.second == 0:
-    cls()
-    prompt = ("Tu est un utilisateur de Twitter, tu parle à tes amis, nous somme le", temps, "dis bonjour à tout le monde et souhaite leur de passer une bonne journée")
-    reponse = GPT(prompt)
-    print(reponse)
-    time.sleep(1)
+
+m1, m2, m3 = minutes_aléatoire()
+
+heure_matin = "08:" + m1
+heure_soir = "18:" + m2
+heure_nuit_semaine = "23:" + m3
+heure_nuit_week_end = "02:" + m3
+
+heure_aléatoire1 = heure_aléatoire()
+heure_aléatoire2 = heure_aléatoire()
+
+################################################ DAILY MESSAGES ################################################
 
 cls()
-dico_prompt = {
-                "dis bonjour à tout le monde et souhaite leur de passer une bonne journée" : 1, 
-               "pause leur une question sur leur activité de la journée" : 2, 
-               "demande comment vas être leur week-end" : 3, 
-               "demande comment se passe leur week-end" : 4, 
-               "demande leur comment se passe leur semaine" : 5,
-               "demande leur se qu'il pense comment va être leur journée" : 6,
-               "souhaite leur bonne chance pour leur journée" : 7,
-               "souhaite leur bonne chance pour leur journée de travail" : 8
-               }
 
-prompt1 = "Tu est un utilisateur de Twitter, fais des messages courts en moyenne 100 caractères. caractère, tu parle à tes amis, nous somme le"
-pl = True
-while pl == True:
-    random_nb = random.randint(0, 7)
-    if random_nb == 3 and jour == 5:
-        pl = False
-    if random_nb == 4 and jour == 6:
-        pl = False
-    if ((random_nb == 7 or random_nb == 8) and (random_nb != 5 or random_   nb != 6)):
-        pl = False 
-    if random_nb != 3 and random_nb != 4:
-        pl = False
-         
-for prompt_du_dico, num in dico_prompt.items():
-    if num == random_nb:
-        prompt2 = prompt_du_dico
-        break
-prompt = prompt1 + " " + str(temps) + " " + prompt2
-reponse = GPT(prompt)
-print(reponse)
+##### Planification des programmes #####
 
+schedule.every().day.at(heure_matin).do(fonction_matin)
+schedule.every().day.at(heure_soir).do(fonction_soir)
+if jour != 4 or jour != 5:
+    schedule.every().day.at(heure_nuit_semaine).do(fonction_nuit)
+else:
+    schedule.every().day.at(heure_nuit_week_end).do(fonction_nuit)
+schedule.every().day.at(heure_aléatoire1).do(fonction_phrases_random)
+schedule.every().day.at(heure_aléatoire2).do(fonction_anime_random)
+
+prompt1 = prompt()
+
+########## Programme Principal #########
+
+while True:
+    
+    schedule.run_pending()
+    time.sleep(1)
+    
