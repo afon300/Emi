962b92613d3c5b77b153b74c1297cf495ef1707d
afon30 <antoinefourneyron@gmail.com>
2024-03-04 13:11:01 +0100

    ajout du README.md

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# LaughWithGPT
-bot X / twitter qui fait des blagues très nul vias chat gpt
+# Emi_Bot
+
+---
+
+###### Projet bot X/Twitter et bot discord
+
+## Informations principales
+Librairies utilisés :
+* `openai` pour chat_gpt
+* `discord` pour l'api de discord
+* `twitter` pour l'api de X
+
+Le programme du bot discord s'exécute dans le 'emi_discord.py' et le bot twitter s'execute dans le 'emi_twitter.py'
+
+
+> [!WARNING]
+> La version de Python doit être au-dessus ou égale à 3.12.1
+
+## Autres Informations
+
+* Cloner le repo via `https://github.com/afon300/Emi.git`
+* Dossier du Projet : `cd Emi`
+* Programme Principal `emi_discord.py` et `emi_twitter.py`
+
+## Composition
+
+* Le programme est composé de deux parties accessibles depuis les **dossiers** en exécutant le 'emi_discord.py' et 'emi_twitter'
+* Le programme est aussi composé de d'un dossier 'keys' qui contient les clé de l'api open_ai, twitter et discord
+* Le fichier *librairies_diyco.p* contient les dictionnaire python necéssaire à la **aux prompts chat gpt** et le *api_gpt.py* contient quant à lui les fonctions nécéssaires à la **l'api de open_ai**
+
+# Merci
--- discord/emi_discord.py
+++ discord/emi_discord.py
@@ -9,10 +9,9 @@ from api_gpt import *
 from librairies_dico import *
 
 intents = discord.Intents.default()
-# Crée une instance du bot avec le préfixe de commande
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-# Événement pour indiquer que le bot est prêt
+
 @bot.event
 async def on_ready():
     print("----------------------")
@@ -21,17 +20,18 @@ async def on_ready():
     print("ID: %s"%bot.user.id)
     print("----------------------")
 
-# Commande pour répondre "True" lorsque l'utilisateur mentionne le bot avec une phrase
 @bot.event
-async def on_message(message): # Vérifie si le bot est mentionné et que ce n'est pas lui-même
+async def on_message(message):
+    if message.author == bot.user:
+        return
 
-    if bot.user.mentioned_in(message) and message.author != bot.user: # Récupère la mention de l'utilisateur
-        author_mention = message.author.mention # Récupère le contenu du message après la mention du bot
-        content = message.content.split('@emi')[1].strip() # Répond "True" à l'utilisateur
-        await message.channel.send(f"{author_mention} True") # Appelle le gestionnaire d'événements on_message normal pour les autres fonctionnalités
+    if bot.user in message.mentions:
+        await message.channel.send(f"Salut {message.author.mention}!")
+        print(message)
+
     await bot.process_commands(message)
 
-token_path = os.path.join(os.getcwd(), "discord", "key_discord.txt")
+token_path = os.path.join(os.getcwd(), "keys", "key_discord.txt")
 print(token_path)
 with open(token_path, 'r') as f:
     clé_chiffré = f.read()
